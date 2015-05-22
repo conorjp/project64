d389c674b63e086427d21221f708b3d2ac314fc8
conor <conor.parrish@colorado.edu>
2015-05-22 16:48:29 -0600

    Initial commit from local git

---
+++ project64/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project64
+//
+//  Created by Conor Parrish on 5/22/15.
+//  Copyright (c) 2015 JoCon Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
